718b53ee0fce9e905835b972a996e5497bbe958c
Dwi Fahni Denni <dfdenni0912@gmail.com>
2022-08-22 02:13:02 +0700

    Refactoring bash script get dependencies terraform modules

--- get-modules-codebuild.sh
+++ get-modules-codebuild.sh
@@ -1,44 +1,129 @@
 #!/usr/bin/env sh
 # -----------------------------------------------------------------------------
-#  Get All Terraform Submodules
+#  Get Terraform Submodules (Inside CodeBuild)
 # -----------------------------------------------------------------------------
 #  Author     : DevOps Engineer (support@devopscorner.id)
 #  License    : Apache v2
 # -----------------------------------------------------------------------------
 set -e
 
-TITLE="TERRAFORM MODULES" # script name
-VER="2.3"                 # script version
+TITLE="TERRAFORM SUBMODULES"           # script name
+VER="2.3"                              # script version
 
-cd ${CODEBUILD_SRC_DIR}/terraform
-rm -rf modules
+PATH_CODEBUILD="${CODEBUILD_SRC_DIR}"
+SUBMODULE_TERRAFORM_COMMUNITY="${CODEBUILD_SRC_DIR}/module_community.lst"
+SUBMODULE_TERRAFORM_OFFICIALS="${CODEBUILD_SRC_DIR}/module_officials.lst"
+PATH_MODULES="${PATH_CODEBUILD}/terraform/modules/providers/aws"
+PATH_MODULES_COMMUNITY="${PATH_MODULES}/community"
+PATH_MODULES_OFFICIALS="${PATH_MODULES}/officials"
 
-mkdir -p modules/providers/aws/community
-mkdir -p modules/providers/aws/officials
+COL_RED="\033[22;31m"
+COL_GREEN="\033[22;32m"
+COL_BLUE="\033[22;34m"
+COL_END="\033[0m"
 
-### ================================= ###
+get_time() {
+  DATE=$(date '+%Y-%m-%d %H:%M:%S')
+}
 
-## Inside `terraform` folder
-SUBMODULE_COMMUNITY="${CODEBUILD_SRC_DIR}/module_community.lst"
-SUBMODULE_OFFICIALS="${CODEBUILD_SRC_DIR}/module_officials.lst"
+print_line0() {
+  echo "$COL_GREEN=====================================================================================$COL_END"
+}
 
-cd modules/providers/aws/community
+print_line1() {
+  echo "$COL_GREEN-------------------------------------------------------------------------------------$COL_END"
+}
 
-while IFS= read line; do
-  # skip_exists $line
-  git clone --depth 1 $line
-  echo ''
-done <"$SUBMODULE_COMMUNITY"
-echo '- COMMUNITY MODULES DOWNLOADED -'
+print_line2() {
+  echo "-------------------------------------------------------------------------------------"
+}
 
-### ================================= ###
+logo() {
+  clear
+  print_line0
+  echo "$COL_RED'########:'########:'########:::'#######:::'######::::'#####:::'########:::'#######:: $COL_END"
+  echo "$COL_RED..... ##:: ##.....:: ##.... ##:'##.... ##:'##... ##::'##.. ##:: ##.... ##:'##.... ##: $COL_END"
+  echo "$COL_RED:::: ##::: ##::::::: ##:::: ##: ##:::: ##: ##:::..::'##:::: ##: ##:::: ##:..::::: ##: $COL_END"
+  echo "$COL_RED::: ##:::: ######::: ########:: ##:::: ##: ##::::::: ##:::: ##: ##:::: ##::'#######:: $COL_END"
+  echo "$COL_RED:: ##::::: ##...:::: ##.. ##::: ##:::: ##: ##::::::: ##:::: ##: ##:::: ##::...... ##: $COL_END"
+  echo "$COL_RED: ##:::::: ##::::::: ##::. ##:: ##:::: ##: ##::: ##:. ##:: ##:: ##:::: ##:'##:::: ##: $COL_END"
+  echo "$COL_RED ########: ########: ##:::. ##:. #######::. ######:::. #####::: ########::. #######:: $COL_END"
+  echo "$COL_RED........::........::..:::::..:::.......::::......:::::.....::::........::::.......::: $COL_END"
+  print_line1
+  echo "$COL_GREEN# $TITLE :: ver-$VER $COL_END"
+}
 
-cd ../officials
+header() {
+  logo
+  print_line0
+  get_time
+  echo "$COL_RED# BEGIN PROCESS..... (Please Wait)  $COL_END"
+  echo "$COL_RED# Start at: $DATE  $COL_END\n"
+}
 
-while IFS= read line; do
-  # skip_exists $line
-  git clone --depth 1 $line
-  echo ''
-done <"$SUBMODULE_OFFICIALS"
-echo '- OFFICIALS MODULE DOWNLOADED -'
+footer() {
+  print_line0
+  get_time
+  echo "$COL_RED# Finish at: $DATE  $COL_END"
+  echo "$COL_RED# END PROCESS.....  $COL_END\n"
+}
 
+skip_exists() {
+  if [[ -f "$1" ]]; then
+    echo ">> Skip for existing file $1 ..."
+  else
+    git clone --depth 1 $1
+  fi
+}
+
+submodule_cleanup(){
+  print_line2
+  get_time
+  echo "$COL_BLUE[ $DATE ] ##### Rebuild Submodule(s): $COL_END"
+
+  cd ${PATH_CODEBUILD}
+  rm -rf modules
+
+  mkdir -p ${PATH_MODULES_COMMUNITY}
+  mkdir -p ${PATH_MODULES_OFFICIALS}
+  echo '- REBUILD DONE -'
+}
+
+submodule_terrafom_community() {
+  print_line2
+  get_time
+  echo "$COL_BLUE[ $DATE ] ##### Download Community Submodule(s): $COL_END"
+  submodule_download $PATH_MODULES_COMMUNITY $SUBMODULE_TERRAFORM_COMMUNITY
+}
+
+submodule_terrafom_officials() {
+  print_line2
+  get_time
+  echo "$COL_BLUE[ $DATE ] ##### Download Officials Submodule(s): $COL_END"
+  submodule_download $PATH_MODULES_OFFICIALS $SUBMODULE_TERRAFORM_OFFICIALS
+}
+
+submodule_download() {
+  cd $1
+  while IFS= read line; do
+    get_time
+    print_line2
+    echo "$COL_GREEN[ $DATE ]       git clone --depth 1 $line $COL_END"
+    print_line2
+    # skip_exists $line
+    git clone --depth 1 $line
+    echo ""
+  done <"$2"
+  echo '- DOWNLOAD DONE -'
+}
+
+main() {
+  header
+  submodule_cleanup
+  submodule_terrafom_community
+  submodule_terrafom_officials
+  footer
+}
+
+### START HERE ###
+main $@
